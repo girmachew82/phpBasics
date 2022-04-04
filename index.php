016bf331c68dd2008f01a597169b909ae6ce041b
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2022-04-04 16:01:50 +0300

    basics

--- index.php
+++ index.php
@@ -1,5 +1,50 @@
 <?php
+/*
+echo "Hello world";
+print("Helooooooooooo world");
+*/
+$name = "Abebe";
+$age = 25;
+echo "Name ".$name;
+echo "<br>";
+echo "Age ".$age;
+echo "<hr>";
+echo "My name is $name and am $age years old";
 
-echo "Hello World";
-echo "\nfrom girmachew ";
+
+//arthmetic (+,-,/,%,*)
+//logiacl(&&,||,!)
+//relational (<,>,<=,>=,==,!=)
+//assignment =
+//compound assignment +=,-=
+//array operator
+//string operator
+echo "<br>";
+$x=8;
+$y=9;
+echo $x." + ".$y." = ".($x+$y);
+echo "<br>";
+//conditional if , if---else, switch
+$role ="hod";
+$dept ="IS";
+if($role == "admin" || ($role == "hod" && $dept=="IS") )
+{
+echo "Welcome ".$role;
+}
+
+else{
+    echo 'Student';
+}
+//switch
+
+switch($role)
+{
+    case "admin":
+        echo "Welcome ".$role;
+     break;
+     case "hod":
+        echo "Welcome ".$role;
+     break; 
+             
+}
 ?>
